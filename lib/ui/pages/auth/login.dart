e1608472eea707f738c567e5b380f1d45e588e61
Gusti <gustiranda3013@gmail.com>
2023-06-06 15:06:46 +0800

    :lipstick: Update login form sizing

--- lib/ui/pages/auth/login.dart
+++ lib/ui/pages/auth/login.dart
@@ -21,166 +21,164 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: SafeArea(
-            child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 85,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 85,
+            ),
+            Center(
+              child: Text(
+                "Pilih Tipe Akun !",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  color: cotech,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                )),
               ),
-              Center(
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  AccountButton(
+                    imageAssets: "assets/images/icons/user_ic.png",
+                    label: 'Umum',
+                    isActive: isUserActive,
+                    onTap: () {
+                      setState(() {
+                        isUserActive = true;
+                        isTechnicianActive = false;
+                      });
+                    },
+                  ),
+                  AccountButton(
+                    imageAssets: "assets/images/icons/worker_ic.png",
+                    label: 'Teknisi',
+                    isActive: isTechnicianActive,
+                    onTap: () {
+                      setState(() {
+                        isUserActive = false;
+                        isTechnicianActive = true;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            SizedBox(
+              width: 224,
+              child: Center(
                 child: Text(
-                  "Pilih Tipe Akun !",
+                  "Hai ${isTechnicianActive ? 'Teknisi' : 'Kamu'} ! \nSilahkan isi Formulir di bawah",
                   textAlign: TextAlign.center,
                   style: GoogleFonts.poppins(
                       textStyle: const TextStyle(
-                    color: cotech,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                    color: Color(cotechSecondaryValue),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
                   )),
                 ),
               ),
-              const SizedBox(
-                height: 32,
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    AccountButton(
-                      imageAssets: "assets/images/icons/user_ic.png",
-                      label: 'Umum',
-                      isActive: isUserActive,
-                      onTap: () {
-                        setState(() {
-                          isUserActive = true;
-                          isTechnicianActive = false;
-                        });
-                      },
-                    ),
-                    AccountButton(
-                      imageAssets: "assets/images/icons/worker_ic.png",
-                      label: 'Teknisi',
-                      isActive: isTechnicianActive,
-                      onTap: () {
-                        setState(() {
-                          isUserActive = false;
-                          isTechnicianActive = true;
-                        });
-                      },
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              SizedBox(
-                width: 224,
-                child: Center(
-                  child: Text(
-                    "Hai ${isTechnicianActive ? 'Teknisi' : 'Kamu'} ! \nSilahkan isi Formulir di bawah",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                        textStyle: const TextStyle(
-                      color: Color(cotechSecondaryValue),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                    )),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  CustomTextField(
+                    controller: _emailLoginController,
+                    hint: 'e-Mail',
+                    type: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      controller: _emailLoginController,
-                      hint: 'e-Mail',
-                      type: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    CustomTextField(
-                      controller: _passwordLoginController,
-                      hint: 'Password',
-                      isPassword: true,
-                      type: TextInputType.visiblePassword,
-                      textInputAction: TextInputAction.done,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Lupa Password?',
-                            style: TextStyle(color: Colors.black),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    FilledButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                          minimumSize: MaterialStatePropertyAll(
-                              Size(MediaQuery.of(context).size.width, 48)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(8))),
-                          backgroundColor: MaterialStatePropertyAll(
-                              _passwordLoginController.value.text.isNotEmpty &&
-                                      _emailLoginController
-                                          .value.text.isNotEmpty
-                                  ? cotech
-                                  : const Color(0xFFF4F4F4))),
-                      child: Text(
-                        "Masuk",
-                        style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                          color: _passwordLoginController
-                                      .value.text.isNotEmpty &&
-                                  _emailLoginController.value.text.isNotEmpty
-                              ? Colors.white
-                              : const Color(cotechSecondaryValue),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                        )),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Belum Punya Akun?"),
-                    TextButton(
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomTextField(
+                    controller: _passwordLoginController,
+                    hint: 'Password',
+                    isPassword: true,
+                    type: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.done,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
                         onPressed: () {},
                         child: const Text(
-                          'Daftar',
+                          'Lupa Password?',
                           style: TextStyle(color: Colors.black),
-                        ))
-                  ],
-                ),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        minimumSize: MaterialStatePropertyAll(
+                            Size(MediaQuery.of(context).size.width, 48)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8))),
+                        backgroundColor: MaterialStatePropertyAll(
+                            _passwordLoginController.value.text.isNotEmpty &&
+                                    _emailLoginController.value.text.isNotEmpty
+                                ? cotech
+                                : const Color(0xFFF4F4F4))),
+                    child: Text(
+                      "Masuk",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        color: _passwordLoginController.value.text.isNotEmpty &&
+                                _emailLoginController.value.text.isNotEmpty
+                            ? Colors.white
+                            : const Color(cotechSecondaryValue),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      )),
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-        )),
-      ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Belum Punya Akun?"),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Daftar',
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
