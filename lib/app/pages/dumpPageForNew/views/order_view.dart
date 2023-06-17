bcccc54ac90df80e10e14840226ccaf7efc52c2a
Gusti <gustiranda3014@gmail.com>
2023-06-18 01:28:03 +0800

    :lipstick: Create dump page

---
+++ lib/app/pages/dumpPageForNew/views/order_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:teknisi_app/app/pages/main/controllers/main_page_controller.dart';
+
+class OrderView extends GetView<MainPageController> {
+  const OrderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
