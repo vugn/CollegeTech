d905eef9a42355f683b80a74829658f17b7b4414
Gusti <gustiranda3014@gmail.com>
2023-06-18 02:50:43 +0800

    :lipstick: Add Tabbar for order

--- lib/app/pages/order/controllers/order_controller.dart
+++ lib/app/pages/order/controllers/order_controller.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class OrderController extends GetxController {}
+class OrderController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  late Rx<TabController> tabBarController;
+  RxInt currentTabIndex = 0.obs;
+
+  @override
+  void onInit() {
+    tabBarController = TabController(vsync: this, length: 2).obs;
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    tabBarController.value.dispose();
+    super.onClose();
+  }
+}
--- lib/app/pages/order/views/order_view.dart
+++ lib/app/pages/order/views/order_view.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:teknisi_app/app/pages/main/controllers/main_page_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:teknisi_app/app/pages/order/controllers/order_controller.dart';
+import 'package:teknisi_app/app/utils/color_palette.dart';
 
-class OrderView extends GetView<MainPageController> {
+class OrderView extends GetView<OrderController> {
   const OrderView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: SafeArea(
+          child: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                    SliverToBoxAdapter(
+                      child: Column(
+                        children: <Widget>[
+                          Column(children: [
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Container(
+                              height: 52,
+                              margin: const EdgeInsets.all(16),
+                              child: Container(
+                                margin: const EdgeInsets.all(4),
+                                child: Obx(() => TabBar(
+                                      onTap: (value) => controller
+                                          .currentTabIndex.value = value,
+                                      controller:
+                                          controller.tabBarController.value,
+                                      indicator: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8)),
+                                      labelColor: Colors.white,
+                                      labelStyle: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 12)),
+                                      unselectedLabelColor:
+                                          const Color(0xFF494949),
+                                      unselectedLabelStyle: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 12)),
+                                      overlayColor:
+                                          const MaterialStatePropertyAll(
+                                              Colors.transparent),
+                                      tabs: [
+                                        Tab(
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: controller
+                                                            .currentTabIndex
+                                                            .value ==
+                                                        0
+                                                    ? cotech
+                                                    : const Color(0xFFD9D9D9),
+                                                boxShadow: [
+                                                  controller.tabBarController
+                                                              .value.index ==
+                                                          0
+                                                      ? BoxShadow(
+                                                          offset: const Offset(
+                                                              0, 2),
+                                                          blurRadius: 12,
+                                                          color:
+                                                              cotech.shade100)
+                                                      : const BoxShadow()
+                                                ],
+                                                borderRadius:
+                                                    BorderRadius.circular(8)),
+                                            child: const Align(
+                                              alignment: Alignment.center,
+                                              child: Text("Order"),
+                                            ),
+                                          ),
+                                        ),
+                                        Tab(
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: controller
+                                                            .currentTabIndex
+                                                            .value ==
+                                                        1
+                                                    ? cotech
+                                                    : const Color(0xFFD9D9D9),
+                                                boxShadow: [
+                                                  controller.tabBarController
+                                                              .value.index ==
+                                                          1
+                                                      ? BoxShadow(
+                                                          offset: const Offset(
+                                                              0, 2),
+                                                          blurRadius: 12,
+                                                          color:
+                                                              cotech.shade100)
+                                                      : const BoxShadow()
+                                                ],
+                                                borderRadius:
+                                                    BorderRadius.circular(8)),
+                                            child: const Align(
+                                              alignment: Alignment.center,
+                                              child: Text("Jadwal"),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                              ),
+                            ),
+                          ])
+                        ],
+                      ),
+                    )
+                  ],
+              body: Container(
+                margin: const EdgeInsets.all(24),
+                child: Obx(() => TabBarView(
+                        physics: const NeverScrollableScrollPhysics(),
+                        controller: controller.tabBarController.value,
+                        children: [
+                          Center(
+                            child: Text('TAB 1'),
+                          ),
+                          Center(
+                            child: Text('TAB 2'),
+                          ),
+                        ])),
+              ))),
     );
   }
 }
