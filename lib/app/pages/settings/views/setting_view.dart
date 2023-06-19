6d4876213d5880ea54225beee2566c4c845b6d96
Gusti <gustiranda3014@gmail.com>
2023-06-19 15:09:41 +0800

    :lipstick: Add Setting UI

---
+++ lib/app/pages/settings/views/setting_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:teknisi_app/app/pages/settings/controllers/setting_controller.dart';
+
+class SettingView extends GetView<SettingController> {
+  const SettingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
