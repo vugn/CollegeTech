da9115962ce0436b1d168cb048e6ec2a5dd406f6
Gusti <gustiranda3014@gmail.com>
2023-06-21 15:04:34 +0800

    :lipstick: Add Brand list

--- lib/app/pages/order/views/brand_list_view.dart
+++ lib/app/pages/order/views/brand_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,7 +14,71 @@ class BrandsList extends GetView<BrandsController> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            IconButton(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () {
+                  Get.back();
+                },
+                icon: const Icon(CupertinoIcons.back)),
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "Pilh Brand Yang Ingin Diperbaiki",
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w600)),
+              ),
+            ),
+            const SizedBox(
+              height: 21,
+            ),
+            SingleChildScrollView(
+                child: GridView.count(
+              shrinkWrap: true,
+              physics: const BouncingScrollPhysics(),
+              crossAxisCount: 2,
+              childAspectRatio: 1.65,
+              children: List.generate(
+                  controller.brandResult.brands.length,
+                  (index) => Container(
+                        margin: const EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Stack(
+                            children: [
+                              CachedNetworkImage(
+                                imageUrl: controller.brandResult.brands[index]
+                                    ['image'],
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                                placeholder: (context, url) => const Center(
+                                  child: CircularProgressIndicator(),
+                                ),
+                              ),
+                              Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () {},
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )),
+            ))
+          ],
         ),
       ),
     );
